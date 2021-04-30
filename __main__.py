c8737b10c994b8a7fb00aecc794dda03138e73a6
D-thalagala <83451115+D-thalagala@users.noreply.github.com>
2021-04-30 16:20:47 +0530

    Devinbot

---
+++ __main__.py
@@ -0,0 +1,14 @@
+start_text = """
+song down by [{}](tg://user?id={}) 
+I'M Music Bot By @devin_thalagala
+any song🥰🤗🥰
+
+"""
+
+owner_help = """
+/blacklist user_id
+/unblacklist user_id
+/broadcast message to send
+/eval python code
+/chatlist get list of all chats
+"""
